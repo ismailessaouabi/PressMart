b03d0d1d1d7fa1231400bcac6da1b58acb2e4bc6
ismail <votre_email@example.com>
2025-04-14 13:41:23 +0100

    update1

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+        <h1>PressMart.</h1>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Shope</a></li>
+            <li><a href="#">Pages</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Elements</a></li>
+            <li><a href="#">Buy</a></li>
+        </ul>
+
+        <div class="icons">
+            <i class="fa-regular fa-user"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-solid fa-cart-plus"></i>
+        </div>
+    </header>
+
+    <main>
+        <div class="main-content">
+            <h1>MEN'S FASHION</h1>
+            <P>Min. 35-70% Off</P>
+            <div class="btns-container">
+                <button>Shope now</button>
+                <button>Read more</button>
+            </div>
+        </div>
+        
+        <img src="/images/person1.png" alt="">
+        
+    </main>
+
+    <section class="bar">
+        <div class="bar-item">
+            <i class="fa-solid fa-truck-fast"></i>
+            <div class="bar-content">
+                <h3>Free shipping</h3>
+                <p>On All orders drow 99$</p>
+            </div>
+        </div>
+
+        <div class="bar-item">
+            <i class="fa-solid fa-truck-fast"></i>
+            <div class="bar-content">
+                <h3>Free shipping</h3>
+                <p>On All orders drow 99$</p>
+            </div>
+        </div>
+
+        <div class="bar-item">
+            <i class="fa-solid fa-truck-fast"></i>
+            <div class="bar-content">
+                <h3>Free shipping</h3>
+                <p>On All orders drow 99$</p>
+            </div>
+        </div>
+
+        <div class="bar-item">
+            <i class="fa-solid fa-comments"></i>
+            <div class="bar-content">
+                <h3>Free shipping</h3>
+                <p>On All orders drow 99$</p>
+            </div>
+        </div>
+    </section>
+
+    <script src="scripte.js"></script>
+</body>
+</html>
